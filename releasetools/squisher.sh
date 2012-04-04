2e3df6448bccf3a151afc76d8292686cd12eb096
SunRain <5908183@163.com>
2012-04-04 15:06:12 +0800

    we do not use the stock init binary in 2nd-init method, we use the built init binary
    Change-Id: I89bb3c562596f082e734cfde48f304b695a67689

--- releasetools/squisher.sh
+++ releasetools/squisher.sh
@@ -15,10 +15,9 @@ mkdir -p $REPACK/ota/system/etc/terminfo/x
 cp $REPACK/ota/system/etc/terminfo/l/linux $REPACK/ota/system/etc/terminfo/x/xterm
 
 # prebuilt boot, devtree, logo & updater-script
-#cp -f $ANDROID_BUILD_TOP/vendor/motorola/spyder/boot-222-179-2.smg $REPACK/ota/boot.img
-#cp -f $ANDROID_BUILD_TOP/vendor/motorola/spyder/devtree-222-179-2.smg $REPACK/ota/devtree.img
-#cp -f $ANDROID_BUILD_TOP/vendor/motorola/spyder/logo-moto.raw $REPACK/ota/logo.img
 cp -f $ANDROID_BUILD_TOP/device/motorola/spyder/updater-script $REPACK/ota/META-INF/com/google/android/updater-script
 #if [ -n "$CYANOGEN_RELEASE" ]; then
 #  cat $ANDROID_BUILD_TOP/device/motorola/spyder/updater-script-rel >> $REPACK/ota/META-INF/com/google/android/updater-script
 #fi
+
+cp -f $ANDROID_BUILD_TOP/out/target/product/spyder/root/init $REPACK/ota/system/bootmenu/2nd-init/init
